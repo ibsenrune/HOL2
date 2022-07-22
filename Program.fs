9917a3f10617c3fd3867d47f97e687829037d694
Rune Ibsen <rune@runeibsen.dk>
2022-07-22 12:27:00 +0000

    Clean up in Program.fs

--- Program.fs
+++ Program.fs
@@ -13,18 +13,7 @@ let getOk = function
   
 [<EntryPoint>]
 let main argv =
-  //let ast = parse program "let f = fun x -> fun y -> x*y in f 3 4"
-  //let ast = parse program "let f = fun x -> fun y -> x*y in f 3 4"
   let ast = parse program "((fun x -> fun y -> x*y) 3 4)+2" //virker ikke
-  //let ast = parse program "(fun y -> let x = 3 in x*y) 4" //virker
-  //let ast = parse program "let x = 3 in (fun y -> x*y) 4" //virker
-  //let ast = parse program "(fun y -> x*y) 4"
-  //let ast = parse program "let fac = fun x -> if x then x*(fac (x-1)) else 1 in fac 3"
-  //let ast = parse program "if foo then 1 else 2"
-  //let ast = parse program "let i = 5 in (fun x -> i*x) 4"
-  //let ast = parse program "(fun x -> fun y -> x*y)" |> getOk
-  //output ast
-  //let res = Result.bind (Interpreter.interpret (Map.ofList [("x", Interpreter.Value.Integer 6)])) ast
   let res = Result.bind Interpreter.interpret' ast
   //let res = closedIn 
   //            (LetIn("x", Integer 3, Multiply ((Identifier "x"), (Identifier "x"))))

0bb94b36ae486637dcb416e64b250e3530412493
Rune Ibsen <rune@runeibsen.dk>
2022-07-22 12:23:31 +0000

    Implement closures

--- Interpreter.fs
+++ Interpreter.fs
@@ -3,10 +3,15 @@ open Ast
 
 type Value =
   | Integer of int
-  | Function of string * Expression
-type Environment = Map<string, Value>
+  | Closure of string * Expression * Environment
+and Environment = Map<string, Value>
+
+let combineEnvironments (inner : Environment) (outer : Environment) : Environment =
+  ((Map.toList inner), (Map.toList outer)) ||> List.append |> Map.ofList
 
 let rec interpret (env : Environment) (expr : Expression) : Result<Value,string> =
+  //printfn "%A in %A" expr env
+  //printfn "----"
   let interEnv = interpret env
   let binaryArithmetic op lhs rhs =
     match interEnv lhs, interEnv rhs with
@@ -16,11 +21,11 @@ let rec interpret (env : Environment) (expr : Expression) : Result<Value,string>
     | Error(s), Error(s') -> Error(sprintf "%s %s" s s')
     | _ as e -> Error(sprintf "Arithmetic operations are not supported for non-integer types: %A" e)
   match expr with
-  | Call(f,a) ->
+  | Call(f, a) ->
       match interpret env f with
-      | Ok(Function(p,e)) -> interpret env (LetIn(p, a, e))
+      | Ok(Closure(p,b,e)) -> interpret (combineEnvironments e env) (LetIn(p, a, b))
       | _ -> Error (sprintf "%A is not callable" f)
-  | Expression.Function(param, e) -> Ok(Value.Function(param, e))
+  | Expression.Function(param, e) -> Ok(Value.Closure(param, e, env))
   | IfThenElse(c, e1, e2) ->
     let c' = interEnv c
     match c' with
@@ -32,7 +37,7 @@ let rec interpret (env : Environment) (expr : Expression) : Result<Value,string>
       match value with
       | Ok(v) -> 
         let env' = Map.add name v env
-        interpret env' body
+        interpret env' body //Her er problemet
       | Error(s) -> Error(s)
   | Add(lhs, rhs) -> binaryArithmetic (+) lhs rhs
   | Subtract(lhs, rhs) -> binaryArithmetic (-) lhs rhs
